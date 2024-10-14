8b11dce19057e9345e82523ef50940d6d927c3cc
Amanda Paul Dull <118822887+pdamanda@users.noreply.github.com>
2024-10-14 01:56:55 +0000

    Alterado o layout e corrigido o código

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 document.getElementById("button").addEventListener("click", () => {
-    console.log("passou")  
-        const url = document.getElementById("input").value;
-    
-      fetch("https://api.encurtador.dev/encurtamentos", {
+    const url = document.getElementById("input").value;
+
+    fetch("https://api.encurtador.dev/encurtamentos", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({ url: url }),
-      })
+    })
         .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("API request failed");
-          }
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("API request failed");
+            }
         })
         .then((data) => {
-          document.getElementById("text").innerHTML = data.urlEncurtada;
-    
-          console.log("DADOS DA API", data);
+            document.getElementById("text").innerHTML = data.urlEncurtada;
+
+            console.log("DADOS DA API", data);
         })
-    
+
         .catch((error) => {
-          console.log("ERRO", error);
+            console.log("ERRO", error);
         });
-    });
-    
+});
